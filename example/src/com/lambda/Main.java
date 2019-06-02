72ebb830b61ceceb45fbf9032366052405f044b3
Leslie <rleslie1015@gmail.com>
2019-06-01 21:35:36 -0500

    intellij practice

--- example/src/com/lambda/Main.java
+++ example/src/com/lambda/Main.java
@@ -2,7 +2,7 @@ package com.lambda;
 
 public class Main
 {
-
+    //running first java program in intellij!
     public static void main(String[] args)
     {
         System.out.println("Running...");
